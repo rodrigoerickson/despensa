0cd54ea4d81c67f137e1fa39a5b14709a29b18b1
Rodrigo Erickson <cin_rosilva@uolinc.com>
2020-07-06 18:31:49 -0300

    criação do componente de botão

--- src/components/section-list/section/sub-section/sub-section.jsx
+++ src/components/section-list/section/sub-section/sub-section.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import Button from '../../../shared/button/button'
 
 function labelSubSection(props){
     return props.subSections.map(val =>{
-        return (<div>{val.label}</div>)
+        return (<div>
+                {val.label}
+                <Button></Button>
+                </div>)
     })
 }
 
 function SubSection(props) {
-    return (<div>{labelSubSection(props)}</div>);
+    return (
+        <div>
+            {labelSubSection(props)}
+        </div>
+        );
 }
 
 export default SubSection;
---
+++ src/components/shared/button/button.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import './button.css'
+
+export default props =>
+    <button className='button'>0</button>
